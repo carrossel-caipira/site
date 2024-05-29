ca8eb1fe4c704825ad751e5a95748cb4704e9f98
Arthur Alves Ribeiro Gordo <arthur.gordo@unesp.br>
2024-05-29 08:34:38 -0300

    Variaveis espacamento | task#113 [Criar estilos globais]

--- src/assets/styles/themes/default.jsx
+++ src/assets/styles/themes/default.jsx
@@ -4,4 +4,12 @@ export default {
   titleColor: '#63B3D2',
   subTitleColor: '#454545',
   paragraphColor: '#6B6B6B',
+
+  // spacing
+  margin_xs: ".236em",
+  margin_s: ".272em",
+  margin_m: ".618em",
+  margin_l: "1em",
+  margin_xl: "1.618em",
+  margin_xxl: "2.618em",
 }
