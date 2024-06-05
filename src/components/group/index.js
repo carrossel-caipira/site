c1cb2ef7569290862b1976ab7eaa0ab90e2a5114
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2024-06-05 16:45:44 -0300

    Add Equipe | task#121 [Componente Grouo]

---
+++ src/components/group/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './style.css';
+import claytonImage from './fotos/clayton.png';
+import dogImage from './fotos/dog.png';
+
+const perfis = [
+    { nome: 'Clayton', imagem: claytonImage },
+    { nome: 'Clayton', imagem: claytonImage },
+    { nome: 'Clayton', imagem: claytonImage },
+    { nome: 'dog', imagem: dogImage },
+    { nome: 'dog', imagem: dogImage },
+    { nome: 'dog', imagem: dogImage },
+    { nome: 'Clayton', imagem: claytonImage },
+    { nome: 'Clayton', imagem: claytonImage },
+    { nome: 'Clayton', imagem: claytonImage },
+    { nome: 'dog', imagem: dogImage },
+    { nome: 'dog', imagem: dogImage },
+    { nome: 'dog', imagem: dogImage },
+];
+
+function ListaPerfis() {
+    const [indiceInicial, setIndiceInicial] = useState(0);
+  
+    function rolar(direcao) {
+      let novoIndice = indiceInicial;
+      novoIndice = direcao === 'esquerda'
+        ? Math.max(novoIndice - 1, 0)
+        : Math.min(novoIndice + 1, perfis.length - 3);
+  
+      setIndiceInicial(novoIndice);
+    }
+
+  return (
+    <div className="container">
+      <h2 className="titulo">EQUIPE</h2>
+      <button onClick={() => rolar('esquerda')} className="seta">&lt;</button>
+      <div className="perfis">
+        {perfis.slice(indiceInicial, indiceInicial + 3).map((perfil, index) => (
+          <div key={perfil.nome + index} className="perfil">
+            <img src={perfil.imagem} alt={perfil.nome} />
+            <p>{perfil.nome}</p>
+          </div>
+        ))}
+      </div>
+      <button onClick={() => rolar('direita')} className="seta">&gt;</button>
+    </div>
+  );
+}
+
+export default ListaPerfis;
