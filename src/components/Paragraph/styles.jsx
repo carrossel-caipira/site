2a63e123cb570b8090f1efb3ccc02dda51f3b086
argArthur <46555016+argArthur@users.noreply.github.com>
2024-06-06 02:22:56 -0300

    concertando espacamento relativo | task#118 [Componentes basicos]

--- src/components/Paragraph/styles.jsx
+++ src/components/Paragraph/styles.jsx
@@ -3,5 +3,5 @@ import styled from 'styled-components'
 export const Paragraph = styled.p`
     color: ${props => props.theme.paragraphColor};
     font-family: Poppins;
-    font-size: 1em;
+    font-size: 1rem;
 `
--- src/components/Title/styles.jsx
+++ src/components/Title/styles.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const Title = styled.h1`
   font-family: "Bowlby One SC", sans-serif;
-  font-size: 4.235em;
+  font-size: 4.235rem;
   color: ${props => props.theme.titleColor};
   letter-spacing: 3px;
   line-height: 1.1;
