bc30d267009b887e3e6e3271d2ff95f399eeb962
Murilo Gonzaga <murilogonzaga115@gmail.com>
2024-07-10 19:37:08 -0300

    responsividade navbar + inicio do menu hamburguer para telas menores | task #81 [Componente NavBar]

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import logo from '../../assets/images/logo_carrossel.svg';
-import { Nav, LinkItems, Logo, LogoText, NavLink, LogoImage } from './styles.jsx';
+import { Nav, Logo, LogoText, LogoImage, LinkItems, NavLink, Hamburger, MobileMenu } from './styles.jsx';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [Mobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleResize = useCallback(() => {
+    setIsMobile(window.innerWidth < 768);
+    if (window.innerWidth >= 768) {
+      setIsOpen(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [handleResize]);
+
   return (
     <Nav>
       <Logo>
         <LogoImage>
-          <img src={logo} alt="Logo.carrosel" width="77" />
+          <img src={logo} alt="Logo.carrosel" />
         </LogoImage>
         <LogoText>Carrossel Caipira</LogoText>
       </Logo>
+      <Hamburger onClick={toggleMenu}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </Hamburger>
       <LinkItems>
         <NavLink to="group" smooth={true} spy={true} offset={-70} duration={500}>
           Membros
@@ -22,6 +47,17 @@ function Navbar() {
           Contato
         </NavLink>
       </LinkItems>
+      <MobileMenu isOpen={isOpen}>
+        <NavLink onClick={toggleMenu} to="group" smooth={true} spy={true} offset={-70} duration={500}>
+          Membros
+        </NavLink>
+        <NavLink onClick={toggleMenu} to="historia" smooth={true} spy={true} offset={-70} duration={500}>
+          História
+        </NavLink>
+        <NavLink onClick={toggleMenu} to="contato" smooth={true} spy={true} offset={-70} duration={500}>
+          Contato
+        </NavLink>
+      </MobileMenu>
     </Nav>
   );
 }
--- src/components/navbar/styles.jsx
+++ src/components/navbar/styles.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import { Link } from 'react-scroll';
 
 export const Nav = styled.nav`
@@ -9,17 +9,35 @@ export const Nav = styled.nav`
   color: white;
   font-family: Advent Pro;
   width: 100vw;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  position: relative;
+  padding: 0 20px;
+
+  @media (max-width: ${props => props.theme.deviceMobile}) {
+    height: 70px;
+  }
 `;
 
 export const Logo = styled.div`
   display: flex;
-  margin-left: 61px;
   align-items: center;
   gap: 12px;
 
+  @media (max-width: ${props => props.theme.deviceMobile}) {
+    gap: 8px;
+  }
 `;
+
 export const LogoImage = styled.div`
   transition: transform 0.7s ease;
+
+  img {
+    width: 6vw;
+
+    @media (max-width: ${props => props.theme.deviceMobile}) {
+      width: 8vw;
+    }
+  }
 
   &:hover {
     transform: rotate(360deg);
@@ -27,15 +45,22 @@ export const LogoImage = styled.div`
 `;
 
 export const LogoText = styled.span`
-  font-size: 54px;
+  font-size: 3.375vw;
+
+  @media (max-width: ${props => props.theme.deviceMobile}) {
+    font-size: 5vw;
+  }
 `;
 
 export const LinkItems = styled.div`
   display: flex;
   align-items: center;
-  font-size: 30px;
-  gap: 50px;
-  margin-right: 61px;
+  font-size: 1.875vw;
+  gap: 3.125vw;
+
+  @media (max-width: ${props => props.theme.deviceMobile}) {
+    display: none;
+  }
 `;
 
 export const NavLink = styled(Link)`
@@ -43,8 +68,60 @@ export const NavLink = styled(Link)`
   transition: font-size 0.2s ease;
   text-shadow: 1px 2px 1px rgba(0, 0, 0, 0.25);
 
+  @media (max-width: ${props => props.theme.deviceMobile}) {
+    font-size: 2.5vw;
+  }
+
   &:hover {
-    font-size: 32px;
+    font-size: 2vw;
     border-bottom: 2px solid white;
+  }
+`;
+
+export const Hamburger = styled.div`
+  display: none;
+  cursor: pointer;
+  font-size: 25px;
+
+  @media (max-width: ${props => props.theme.deviceMobile}) {
+    display: block;
+  }
+`;
+
+export const MobileMenu = styled.div`
+  display: none;
+  flex-direction: column;
+  align-items: flex-end;
+  position: absolute;
+  top: 70px;
+  right: 0;
+  width: 40%;
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+  background-color: rgba(0, 0, 0, 0.7);
+  /*background-color: ${props => props.theme.darkBg};*/
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  gap: 20px;
+  font-size: 20px;
+  text-transform: uppercase;
+  transition: transform 0.3s ease-in-out;
+
+  ${({ isOpen }) => isOpen && `
+    display: flex;
+    transform: translateX(0);
+  `}
+
+  @media (max-width: ${props => props.theme.deviceMobile}) {
+    display: flex;
+    transform: translateX(${({ isOpen }) => (isOpen ? '0' : '100%')});
+  }
+
+  ${NavLink} {
+    font-size: 20px;
+
+    &:hover {
+      font-size: 22px;
+    }
   }
 `;
