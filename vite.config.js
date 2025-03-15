c9ae045420fedee136ea5878ff7fca9ca9c4801c
ringass <murilogonzaga115@gmail.com>
2025-03-15 16:07:00 -0300

    github pages 2

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/site/', // Nome do repositório
+});
